589b6ed2a0772b3f857695a6bdbe960c1269f865
Lhsales <henri.luhsales@gmail.com>
2021-08-23 00:55:24 -0300

    Criação do LogController

---
+++ SistemaDeAlarmes/SistemaDeAlarmes/Controllers/LogController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeAlarmes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeAlarmes.Controllers
+{
+    public class LogController : Controller
+    {
+        private AlarmeContext db = new AlarmeContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public Log inserirLog(Log log)
+        {
+            log.DataCriacao = DateTime.Now;
+            db.Logs.Add(log);
+            db.SaveChanges();
+            return log;
+        }
+    }
+}
